d95f83fc3b825234a73cfd4c9583afa3bfe80b1d
Alexis Romero <alexis.romero@open-xchange.com>
2023-02-13 12:17:25 +0100

    forcing 2.3.7

--- tasks.py
+++ tasks.py
@@ -45,7 +45,7 @@ auth_build_deps = [    # FIXME: perhaps we should be stealing these from the deb
     'ruby-bundler',
     'ruby-dev',
     'sqlite3',
-    'unixodbc-dev',
+    'unixodbc-dev=2.3.7',
 ]
 rec_build_deps = [
     'libcap-dev',
