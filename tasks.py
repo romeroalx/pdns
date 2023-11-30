329ce97844504a9c15df070b1a4b9b7d9988ea20
romeroalx <alexis.romero@open-xchange.com>
2023-11-30 15:59:42 +0100

    test auth backends including pkcs11

--- tasks.py
+++ tasks.py
@@ -675,7 +675,7 @@ backend_regress_tests = dict(
         'bind-dnssec-nsec3-both',
         'bind-dnssec-nsec3-optout-both',
         'bind-dnssec-nsec3-narrow',
-        # FIXME  'bind-dnssec-pkcs11'
+        'bind-dnssec-pkcs11'
     ],
     geoip = [
         'geoip',
